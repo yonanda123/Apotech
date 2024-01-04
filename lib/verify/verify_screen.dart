cede0c2b127e25d136cfd79a0a7578f10923a094
Yonanda Haryono <yonandaharyono@gmail.com>
2024-01-04 20:04:15 +0700

    Verify Screen

--- lib/verify/verify_screen.dart
+++ lib/verify/verify_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:apotech/register/register_screen.dart';
+import 'package:apotech/verifySucces/verify_succes_screen.dart';
+import 'dart:async';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final double height;
@@ -42,6 +44,42 @@ class VerifyScreen extends StatefulWidget {
 }
 
 class _VerifyScreenState extends State<VerifyScreen> {
+  List<TextEditingController> _controllers =
+      List.generate(6, (index) => TextEditingController());
+  int _minutes = 0;
+  int _seconds = 59;
+
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  void _startTimer() {
+    const oneSecond = Duration(seconds: 1);
+    _timer = Timer.periodic(oneSecond, (timer) {
+      setState(() {
+        if (_minutes == 0 && _seconds == 0) {
+          _timer.cancel();
+        } else if (_seconds == 0) {
+          _minutes -= 1;
+          _seconds = 59;
+        } else {
+          _seconds -= 1;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controllers.forEach((controller) => controller.dispose());
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +99,7 @@ class _VerifyScreenState extends State<VerifyScreen> {
                   fontFamily: 'OverpassExtraBold',
                 ),
               ),
-              SizedBox(height: 24.0),
+              SizedBox(height: 8.0),
               Text(
                 'We just send you a verification code via phone +62 821 39 488 953',
                 style: TextStyle(
@@ -70,6 +108,97 @@ class _VerifyScreenState extends State<VerifyScreen> {
                   fontFamily: 'OverpassLight',
                 ),
                 textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 25.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(
+                  6,
+                  (index) => SizedBox(
+                    width: 48.0,
+                    child: TextFormField(
+                      controller: _controllers[index],
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      maxLength: 1,
+                      decoration: InputDecoration(
+                        counterText: '',
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0),
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        if (value.length == 1 && index < 5) {
+                          FocusScope.of(context).nextFocus();
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 32.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => VerifySuccesScreen(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(311, 50),
+                      primary: Color(0xff4157FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                      elevation: 14.0,
+                      shadowColor: Color(0xff4157FF).withOpacity(0.1),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Text(
+                        'SUBMIT CODE',
+                        style: TextStyle(
+                          color: Color(0xffffffff),
+                          fontSize: 16.0,
+                          fontFamily: 'OverpassExtraBold',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 28.0),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'The verify code will expire in ${_minutes.toString().padLeft(2, '0')}:${_seconds.toString().padLeft(2, '0')}',
+                  style: TextStyle(
+                    color: Color(0x73090F47),
+                    fontSize: 16.0,
+                    fontFamily: 'OverpassLight',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Resend Code',
+                    style: TextStyle(
+                        color: Color(0xff4157FF),
+                        fontSize: 15.0,
+                        fontFamily: 'OverpassLight'),
+                  ),
+                ),
               ),
             ],
           ),
---
+++ lib/verifySucces/verify_succes_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifySuccesScreen extends StatefulWidget {
+  const VerifySuccesScreen({super.key});
+
+  @override
+  State<VerifySuccesScreen> createState() => _VerifySuccesScreenState();
+}
+
+class _VerifySuccesScreenState extends State<VerifySuccesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
